11035e8fdba07834cf633c005cfaa3c74e02077f
James Lee <jameswlee109@gmail.com>
2019-10-08 20:09:05 -0500

    noteful client

---
+++ src/notes-helpers.js
@@ -0,0 +1,15 @@
+
+export const findFolder = (folders=[], folderId) =>
+  folders.find(folder => folder.id === Number(folderId))
+
+export const findNote = (notes=[], noteId) =>
+  notes.find(note => note.id === Number(noteId))
+
+export const getNotesForFolder = (notes=[], folderId) => (
+  (!folderId)
+    ? notes
+    : notes.filter(note => note.folder_id === Number(folderId))
+)
+
+export const countNotesForFolder = (notes=[], folderId) =>
+  notes.filter(note => note.folder_id === folderId).length
